c2a1505def49e1d6294952e2be42cf015948cca2
Nanret123 <nanretgungshik@gmail.com>
2025-01-29 23:21:29 +0100

    feat: worked on the home page

---
+++ frontend/src/pages/home/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Banner from './Banner'
+import Categories from './Categories'
+import Hero from './Hero'
+import TrendingProducts from '../shop/TrendingProducts'
+
+const Home = () => {
+  return (
+    <>
+    <Banner />
+    <Categories />
+    <Hero />
+    <TrendingProducts />
+    </>
+  )
+}
+
+export default Home 
